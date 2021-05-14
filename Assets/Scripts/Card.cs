79999dc23a9c9539c34f337a337bd2b58b651175
tacky-yamamoto <yamamura.dat@gmail.com>
2021-05-14 16:08:33 +0900

    Initial commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    //カードのID
+    public int id;
+
+    // 表示するカードの画像
+    public Image CardImage;
+}
---
+++ Assets/Scripts/CardCreateManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardCreateManager : MonoBehaviour
+{
+    // 生成するCardオブジェクト
+    public Card CardPrefab;
+
+    // 「カード」を生成する親オブジェクト
+    public RectTransform CardCreateParent;
+
+    void Start()
+    {
+
+        Card card = Instantiate<Card>(this.CardPrefab, this.CardCreateParent);
+    }
+}
