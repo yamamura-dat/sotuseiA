4a35e2810bee2302fea3815aa41a3f3e598b6e85
tacky-yamamoto <yamamura.dat@gmail.com>
2021-05-19 12:04:55 +0900

    カード配置、乱数

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -6,8 +6,66 @@ using UnityEngine.UI;
 public class Card : MonoBehaviour
 {
     //カードのID
-    public int id;
+    public int Id;
 
     // 表示するカードの画像
     public Image CardImage;
+
+    // 選択されているか判定
+    private bool mIsSelected = false;
+
+    // カード情報
+    private CardData mData;
+    // カードの設定
+    public void Set(CardData data)
+    {
+        // カード情報を設定
+        this.mData = data;
+
+        // IDを設定する
+        this.Id = data.Id;
+
+        // 表示する画像を設定する
+        // 初回は全て裏面表示とする
+        this.CardImage.sprite = Resources.Load<Sprite>("Image/samplecard");
+
+        // 選択判定フラグを初期化する
+        this.mIsSelected = false;
+    }
+    /// <summary>
+    /// 選択された時の処理
+    /// </summary>
+    public void OnClick()
+    {
+        // カードが表面になっていた場合は無効
+        if(this.mIsSelected)
+        {
+            return;
+        }
+
+        Debug.Log("OnClick");
+
+        // 選択判定フラグを有効にする
+        this.mIsSelected = true;
+
+        // カードを表面にする
+        this.CardImage.sprite = this.mData.ImgSprite;
+    }
+}
+/// <summary>
+/// カードの情報クラス
+/// </summary>
+public class CardData
+{
+    // カードID
+    public int Id { get; private set; }
+
+    // 画像
+    public Sprite ImgSprite { get; private set; }
+
+    public CardData(int _id, Sprite _sprite)
+    {
+        this.Id = _id;
+        this.ImgSprite = _sprite;
+    }
 }
--- Assets/Scripts/CardCreateManager.cs
+++ Assets/Scripts/CardCreateManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq; //このusing を新規に記入しておいてください。
+using System; //同じく
 
 public class CardCreateManager : MonoBehaviour
 {
@@ -12,7 +14,42 @@ public class CardCreateManager : MonoBehaviour
 
     void Start()
     {
+        //Card card = Instantiate<Card>(this.CardPrefab, this.CardCreateParent);
 
-        Card card = Instantiate<Card>(this.CardPrefab, this.CardCreateParent);
+        // カード情報リスト
+        List<CardData> cardDataList = new List<CardData>();
+        // 表示するカード画像情報のリスト
+        List<Sprite> imgList = new List<Sprite>();
+        // Resources/Imageフォルダ内にある画像を取得する
+        imgList.Add(Resources.Load<Sprite>("Image/buki_morningstar_flail"));
+        imgList.Add(Resources.Load<Sprite>("Image/fantasy_dwarf"));
+        imgList.Add(Resources.Load<Sprite>("Image/fantasy_game_character_slime"));
+
+        // forを回す回数を取得する
+        int loopCnt = imgList.Count;
+
+        for(int i =0; i<loopCnt; i++)
+        {
+            // カード情報を生成する
+            CardData cardata = new CardData(i, imgList[i]);
+            cardDataList.Add(cardata);
+        }
+            // 生成したカードリスト２つ分のリストを生成する
+            List<CardData> SumCardDataList = new List<CardData>();
+            SumCardDataList.AddRange(cardDataList);
+            SumCardDataList.AddRange(cardDataList);
+
+            // リストの中身をランダムに再配置する
+            List<CardData> randomCardDataList = SumCardDataList.OrderBy(a => Guid.NewGuid()).ToList();
+
+            // カードオブジェクトを生成する
+            foreach (var _cardData in randomCardDataList)
+            {
+
+                // Instantiate で Cardオブジェクトを生成
+                Card card = Instantiate<Card>(this.CardPrefab, this.CardCreateParent);
+                // データを設定する
+                card.Set(_cardData);
+            }
     }
 }
