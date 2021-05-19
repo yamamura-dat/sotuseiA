93744b3cb0d3806552fe018ee768c76763985fa0
tacky-yamamoto <yamamura.dat@gmail.com>
2021-05-19 12:20:01 +0900

    追加

---
+++ Assets/Scripts/GameStateController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateController : MonoBehaviour
+{
+    // 選択されたカードIDリスト
+    public List<int> SelectedCardIdList = new List<int>();
+
+    // シングルトンの生成
+
+}
